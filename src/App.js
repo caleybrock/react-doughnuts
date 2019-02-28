48be3ff4b6774923585bd77a11d78661edecb5d7
Caley Brock <caleybrock@Caleys-MacBook-Pro.local>
2019-02-27 21:21:18 -0500

    handle events

--- src/App.js
+++ src/App.js
@@ -1,18 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      icing: '#000000',
+      sprinkles: true,
+    };
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("ordering a doughnut");
+  }
+
+  handleIcing = (e) => {
+    console.log(e.target.value);
+  }
+
+  handleSprinkles = (e) => {
+    console.log(e.target.checked);
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Order custom doughnuts</h1>
         <h2>Place your order</h2>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <label htmlFor="icing">Pick your icing colour:</label>
-          <input id="icing" name="icing" type="color"/>
+          <input 
+            id="icing" 
+            name="icing" 
+            type="color" 
+            value={this.state.icing}
+            onChange={this.handleIcing}
+          />
           <label htmlFor="sprinkles">Sprinkles:</label>
-          <input id="sprinkles" name="sprinkles" type="checkbox" />
+          <input 
+            id="sprinkles" 
+            name="sprinkles" 
+            type="checkbox" 
+            checked={this.state.sprinkles}
+            onChange={this.handleSprinkles}  
+          />
           <input type="submit" />
         </form>
 
